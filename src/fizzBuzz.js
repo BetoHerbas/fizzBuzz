e83b7ae0287bbcc190f03c97d118db770b0d58af
BetoHerbas <betitoherbas@gmail.com>
2024-02-22 12:02:50 -0400

    return Fizz if the number is divisible by 3 like 3

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,4 +1,8 @@
 function fizzBuzz(n){
+    n === 3;
+    if(n === 3){
+        return "Fizz";
+    }
     return n;
 }
 export default fizzBuzz;
--- src/fizzBuzz.spec.js
+++ src/fizzBuzz.spec.js
@@ -5,5 +5,8 @@ describe("FizzBuzz", () => {
     it("return the number if isn't divisible by 3 or 5", () => {
         expect(fizzBuzz(3)).toEqual("7");
     });
+    it("return Fizz if the number is divisible by 3 like 3", () => {
+        expect(fizzBuzz(3)).toEqual("Fizz");
+    });
 
 });
